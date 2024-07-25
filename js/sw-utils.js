981a7a811349da1fa7318806eb97410f2d46e2be
Sam <frostrik@gmail.com>
2024-07-25 14:50:28 -0600

    Primer Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+
+//Guarda el cache Dinarmico
+function actualizaCacheDinamico( dynamicCache, req, res) {
+    if( res.ok){
+        return caches.open( dynamicCache).then( cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        })
+    } else {
+        return res;
+    }
+}
